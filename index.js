f337f46b3b922bddfc3cfbecd9e1e8024dd8bc14
wangchen <wangchen6@126.com>
2019-11-17 21:35:27 +0800

    配置打包

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const {app, BrowserWindow} = require('electron')
+const { app, BrowserWindow , ipcMain,  Menu, Tray} = require('electron')
 // electron提供api
 const path = require('path')
 let mainWindow
 
-function createWindow () {
+function createWindow() {
 
   mainWindow = new BrowserWindow({
     width: 1000,
@@ -12,7 +12,7 @@ function createWindow () {
       preload: path.join(__dirname, 'preload.js')
     }
   })
-  
+
 
   // and load the index.html of the app.
   // mainWindow.loadFile('index.html' )
@@ -26,6 +26,32 @@ function createWindow () {
 
 
 app.on('ready', createWindow)
+
+
+
+let appIcon = null
+
+ipcMain.on('put-in-tray', (event) => {
+  const iconName = process.platform === 'win32' ? 'windows-icon.png' : 'iconTemplate.png'
+  const iconPath = path.join(__dirname, iconName)
+  appIcon = new Tray(iconPath)
+
+  const contextMenu = Menu.buildFromTemplate([{
+    label: 'Remove',
+    click: () => {
+      event.sender.send('tray-removed')
+    }
+  }])
+
+  appIcon.setToolTip('Electron Demo in the tray.')
+  appIcon.setContextMenu(contextMenu)
+})
+
+ipcMain.on('remove-tray', () => {
+  appIcon.destroy()
+})
+
+
 
 app.on('window-all-closed', function () {
 
